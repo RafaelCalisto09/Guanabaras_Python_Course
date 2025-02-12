785ea2b0cee58b84f3946fdb1df0bb6c80f2268e
Rafael Calisto <133287670+RafaelCalisto09@users.noreply.github.com>
2025-02-12 06:53:08 +0000

    Add files via upload

---
+++ Desafio004.py
@@ -0,0 +1,4 @@
+n = input('Digite algo aí ')
+print('O tipo primitivo desse valor é: ', type(n))
+print('Esse numero é alpha numerico? ', (n.isalnum()))
+print('Esse numero é alpha alfanumerico? ', (n.isalpha()))
