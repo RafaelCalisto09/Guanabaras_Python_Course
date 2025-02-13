e4fa447d9a8a4d4d9579dd48c58544815457bbc5
Rafael Calisto <133287670+RafaelCalisto09@users.noreply.github.com>
2025-02-13 06:31:29 +0000

    Add files via upload

---
+++ Desafio005.py
@@ -0,0 +1,7 @@
+'''
+Desafio 5
+Faca um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor
+'''
+
+n = int(input('Digite um numero: '))
+print('Analisando o numero, {}, Seu antecessor é {} e seu sucessor é {}.'.format(n, (n-1), (n+1)))
